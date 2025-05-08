b3ba82581e4725d71846406bdc0d27520c0b1134
jiheon <wwlgjs77@gmail.com>
2025-05-08 12:26:02 +0900

    ..

--- src/app/components/Marquee.tsx
+++ src/app/components/Marquee.tsx
@@ -1,8 +1,7 @@
 "use client";
 
 export default function Marquee() {
-  const text =
-    "🌟Lee Ji Heon Portfolio 🌟 010-3015-0928 🌟 wwlgjs77@gmail.com 🌟 ";
+  const text = "🌟Lee Ji Heon Portfolio 🌟 010-3015-0928 🌟 wwlgjs77@gmail.com";
 
   return (
     <div className="marquee-container bg-transparent text-white w-screen fixed top-0 z-9999">
