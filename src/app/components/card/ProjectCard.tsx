d8bf973c165a587c491337355ef667f4af80bf90
Your Name <wwlgjs77@gmail.com>
2025-07-02 20:30:19 +0900

    ..

--- src/app/components/card/ProjectCard.tsx
+++ src/app/components/card/ProjectCard.tsx
@@ -9,13 +9,12 @@ import { projectLinks } from "@/app/components/data/data";
 
 export default function ProjectList() {
   return (
-    <section className="min-h-screen bg-[#21201C] p-16 ">
+    <section className="w-screen min-h-screen bg-[#21201C] p-16 ">
       <div className="grid grid-cols-1 md:grid-cols-3 gap-8 h-full">
         {projectLinks.map((card: ProjectCardProps, idx: number) => (
           <div
             key={idx}
-            className="bg-transparent rounded-lg overflow-hidden shadow-md flex flex-col"
-          >
+            className="bg-gray-900 rounded-lg overflow-hidden shadow-md flex flex-col">
             {/* 카드 이미지 */}
             <div className="relative w-full h-48">
               <Image
@@ -40,8 +39,7 @@ export default function ProjectList() {
                 {card.tags?.map((tag, i) => (
                   <span
                     key={i}
-                    className="px-2 py-1 text-xs bg-gray-600 text-white rounded"
-                  >
+                    className="px-2 py-1 text-xs bg-gray-600 text-white rounded">
                     {tag}
                   </span>
                 ))}
@@ -53,24 +51,21 @@ export default function ProjectList() {
                   href={card.site}
                   target="_blank"
                   rel="noopener noreferrer"
-                  className="px-4 py-2 bg-white text-black font-bold rounded-md hover:bg-gray-400"
-                >
+                  className="px-4 py-2 bg-white text-black font-bold rounded-md hover:bg-gray-400">
                   GO SITE
                 </Link>
                 <Link
                   href={card.velog}
                   target="_blank"
                   rel="noopener noreferrer"
-                  className="px-4 py-2 bg-gray-200 text-gray-800 rounded-md hover:bg-gray-100"
-                >
+                  className="px-4 py-2 bg-gray-200 text-gray-800 rounded-md hover:bg-gray-100">
                   velog
                 </Link>
                 <Link
                   href={card.github}
                   target="_blank"
                   rel="noopener noreferrer"
-                  className="px-4 py-2 bg-gray-200 text-gray-800 rounded-md hover:bg-gray-100"
-                >
+                  className="px-4 py-2 bg-gray-200 text-gray-800 rounded-md hover:bg-gray-100">
                   github
                 </Link>
               </div>
